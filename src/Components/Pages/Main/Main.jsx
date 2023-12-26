ca57185b944c462a69bc567032cdc7c1c22d0e5b
AshikurZaman01 <ashikurzaman774@gmail.com>
2023-12-26 11:24:56 +0600

    add main section side menuBar

--- src/Components/Pages/Main/Main.jsx
+++ src/Components/Pages/Main/Main.jsx
@@ -3,6 +3,21 @@ import './Main.css'
 const Main = () => {
     return (
         <div>
+            <div className='flex justify-center items-start gap-2'>
+
+                <div className='left-section  w-[20%]'>
+                    <div className='flex flex-col justify-start items-start gap-10'>
+                        <button className='buttons active'>All Projects</button>
+                        <button className='buttons'>Tailwind Css</button>
+                        <button className='buttons'>Fontend</button>
+                        <button className='buttons'>FullStack</button>
+                    </div>
+                </div>
+
+                <div className='right-section border w-[70%]'>right</div>
+
+            </div>
+
 
         </div>
     );
