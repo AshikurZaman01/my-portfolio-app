99548e043eb5d5fbd5c64ce2fc49a13e21aee007
AshikurZaman01 <ashikurzaman774@gmail.com>
2023-12-27 18:06:47 +0600

    add scroll btn

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
 
+import { IoIosArrowDropupCircle } from 'react-icons/io'
 import './App.css'
 import Contact from './Components/Pages/Contact/Contact'
 import Footer from './Components/Pages/Footer/Footer'
 import Header from './Components/Pages/Header/Header'
 import Hero from './Components/Pages/Header/Hero/Hero'
 import Main from './Components/Pages/Main/Main'
+import { useEffect, useState } from 'react'
 
 
 function App() {
 
+  const [showScrollBtn, setshowScrollBtn] = useState(false)
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 300) {
+        setshowScrollBtn(true)
+      } else {
+        setshowScrollBtn(false)
+      }
+    })
+  }, [])
+
   return (
-    <div className='container'>
+    <div id='up' className='container'>
       <Header></Header>
       <Hero></Hero>
 
@@ -25,6 +39,13 @@ function App() {
       <div className='divider'></div>
 
       <Footer></Footer>
+
+      {
+        showScrollBtn &&
+        <a href="#up">
+          <button ><IoIosArrowDropupCircle className='scrollTop' /></button>
+        </a>}
+
     </div>
   )
 }
